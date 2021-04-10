44181d9cfa61f58fbb8b4c5f1a317d210ceb046e
LeandroParisi <lepcbelisario@gmail.com>
2021-04-10 13:17:38 -0300

    feat: implements invalid input feedback message

--- app/src/components/Input/Input.jsx
+++ app/src/components/Input/Input.jsx
@@ -2,36 +2,47 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Input = ({
-  title, type, onChange, value, placeholder, isReadOnly,
-}) => (
-  <div className="primaryInput">
-    {title && (
-    <label htmlFor={title}>
-      {title}
-    </label>
-    )}
-    <input
-      id={title}
-      type={type}
-      onChange={onChange && ((e) => onChange(e.target.value))}
-      value={value}
-      readOnly={isReadOnly}
-      placeholder={placeholder}
-    />
-  </div>
-);
+  title, type, onChange, value, placeholder, isReadOnly, onBlur, isValid, errorMessage,
+}) => {
+  console.log('input');
+  console.log(isValid);
+  return (
+    <div className="primaryInput">
+      {title && (
+      <label htmlFor={title}>
+        {title}
+      </label>
+      )}
+      <input
+        id={title}
+        type={type}
+        onChange={onChange && ((e) => onChange(e.target.value))}
+        onBlur={onBlur && ((e) => onBlur(e.target.value))}
+        value={value}
+        readOnly={isReadOnly}
+        placeholder={placeholder}
+        className={!isValid && 'invalid'}
+      />
+      {!isValid && <div className="errorMessage">{ errorMessage }</div>}
+    </div>
+  );
+};
 
 Input.propTypes = {
   title: PropTypes.string,
   type: PropTypes.string.isRequired,
   onChange: PropTypes.func,
+  onBlur: PropTypes.func,
   value: PropTypes.string.isRequired,
   placeholder: PropTypes.string.isRequired,
   isReadOnly: PropTypes.bool,
+  isValid: PropTypes.bool.isRequired,
+  errorMessage: PropTypes.string.isRequired,
 };
 
 Input.defaultProps = {
   onChange: null,
+  onBlur: null,
   isReadOnly: false,
   title: null,
 };
--- app/src/pages/Login/Login.jsx
+++ app/src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import useLoader from '../../hooks/useLoader';
 import logo from '../../assets/logo/logo.png';
-import { validateEmail, validatePassword } from '../../utils/validations';
+import { validateEmail, validatePassword, isInputValid } from '../../utils/validations';
 
 import './Login.scss';
 import Input from '../../components/Input/Input';
@@ -11,6 +11,8 @@ function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isDisabled, setIsDisabled] = useState(true);
+  const [isEmailValid, setIsEmailValid] = useState(null);
+  const [isPasswordValid, setIsPasswordValid] = useState(null);
 
   useEffect(() => {
     setIsDisabled(!(validateEmail.isValid(email) && validatePassword.isValid(password)));
@@ -23,8 +25,24 @@ function Login() {
         <img src={logo} alt="app logo" />
       </header>
       <div className="inputsContainer">
-        <Input type="text" placeholder="Your email" value={email} onChange={setEmail} />
-        <Input type="text" placeholder="Your password" value={password} onChange={setPassword} />
+        <Input
+          type="text"
+          placeholder="Your email"
+          value={email}
+          onChange={setEmail}
+          onBlur={isInputValid(setIsEmailValid, validateEmail.isValid)}
+          isValid={isEmailValid}
+          errorMessage={validateEmail.errorMessage}
+        />
+        <Input
+          type="text"
+          placeholder="Your password"
+          value={password}
+          onChange={setPassword}
+          onBlur={isInputValid(setIsPasswordValid, validatePassword.isValid)}
+          isValid={isPasswordValid}
+          errorMessage={validatePassword.errorMessage}
+        />
       </div>
       <Button title="Login" isDisabled={isDisabled} />
     </main>
