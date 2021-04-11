9cdd9a0d1403451b2b4c211a22339b648ec67a02
LeandroParisi <lepcbelisario@gmail.com>
2021-04-10 22:14:28 -0300

    feat: implements filter to sidebar

--- app/src/components/Sidebar/Sidebar.jsx
+++ app/src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-to-interactive-role */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import './Sidebar.scss';
 
 const options = [
@@ -22,32 +22,36 @@ const options = [
 
 export default function Sidebar({ isSidebarOpen, setIsSidebarOpen }) {
   const history = useHistory();
+  const { pathname } = useLocation();
   const clicked = !isSidebarOpen && 'move-menu';
+
+  const filteredOptions = options.filter((opt) => opt.redirect !== pathname);
 
   return (
     <nav className={`${clicked}`}>
       <ul>
-        {options.map(({ name, redirect }, index) => (
-          <>
-            <li
-              key={name}
-              onClick={() => {
-                setIsSidebarOpen(false);
-                history.push(`${redirect}`);
-              }}
-              className="menu-item"
-              tabIndex={0}
-              role="button"
-              onKeyDown={() => {
-                setIsSidebarOpen(false);
-                history.push(`${redirect}`);
-              }}
-            >
-              { name }
-            </li>
-            { (index < options.length - 1) && <hr /> }
-          </>
-        ))}
+        {filteredOptions
+          .map(({ name, redirect }, index) => (
+            <>
+              <li
+                key={name}
+                onClick={() => {
+                  setIsSidebarOpen(false);
+                  history.push(`${redirect}`);
+                }}
+                className="menu-item"
+                tabIndex={0}
+                role="button"
+                onKeyDown={() => {
+                  setIsSidebarOpen(false);
+                  history.push(`${redirect}`);
+                }}
+              >
+                { name }
+              </li>
+              { (index < filteredOptions.length - 1) && <hr /> }
+            </>
+          ))}
       </ul>
     </nav>
   );
