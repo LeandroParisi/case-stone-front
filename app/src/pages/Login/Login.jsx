4a05c4171f0ad404ba2a189854f795c4b024dcea
LeandroParisi <lepcbelisario@gmail.com>
2021-04-12 23:48:04 -0300

    refactor: extracts userForm to reuse

--- app/src/pages/Login/Login.jsx
+++ app/src/pages/Login/Login.jsx
@@ -2,25 +2,13 @@ import React from 'react';
 import { useHistory } from 'react-router';
 import { toast } from 'react-toastify';
 import useLoader from '../../hooks/useLoader';
-// import { validateEmail, validatePassword, isInputValid } from '../../utils/validations';
 import './Login.scss';
-// import Input from '../../components/Input/Input';
-// import Button from '../../components/Button/Button';
 import postLogin from '../../services/postLogin';
 import { saveToken } from '../../store/localStorage/actions';
 import UserForm from '../../components/UserForm/UserForm';
 
 function Login() {
-  // const [email, setEmail] = useState('');
-  // const [password, setPassword] = useState('');
-  // const [isDisabled, setIsDisabled] = useState(true);
-  // const [isEmailValid, setIsEmailValid] = useState(null);
-  // const [isPasswordValid, setIsPasswordValid] = useState(null);
   const history = useHistory();
-
-  // useEffect(() => {
-  //   setIsDisabled(!(validateEmail.isValid(email) && validatePassword.isValid(password)));
-  // }, [email, password]);
 
   const handleButtonClick = async (email, password) => {
     const { token, message } = await postLogin({ email, password });
