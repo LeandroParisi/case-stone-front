e3c257aa811ae7565bf90a11ea670376a85a040f
LeandroParisi <lepcbelisario@gmail.com>
2021-04-12 21:20:08 -0300

    refactor: extract HeadersIcons to be reused

--- app/src/components/SearchBar/SearchBar.jsx
+++ app/src/components/SearchBar/SearchBar.jsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBook, faUser } from '@fortawesome/free-solid-svg-icons';
 import Input from '../Input/Input';
 import './SearchBar.scss';
+import HeadersIcons from '../HeaderIcons/HeadersIcons';
 
 function SearchBar({
   payloads: {
@@ -21,24 +22,11 @@ function SearchBar({
     <div className="searchContainer">
       <Input className="searchInput" placeholder={placeHolderText} value={searchQuery} onChange={setSearchQuery} onFocus={() => openSearchSideBar} />
       <div className="searchTypeContainer">
-        <div
-          className="searchIconContainer"
-          onClick={() => setSearchCategory('characters')}
-          role="button"
-          tabIndex={0}
-          onKeyDown={() => setSearchCategory('characters')}
-        >
-          <FontAwesomeIcon icon={faUser} color={searchCategory === 'characters' ? '#F2D335' : 'white'} className="searchIcon book" />
-        </div>
-        <div
-          className="searchIconContainer"
-          onClick={() => setSearchCategory('comics')}
-          role="button"
-          tabIndex={0}
-          onKeyDown={() => setSearchCategory('comics')}
-        >
-          <FontAwesomeIcon icon={faBook} color={searchCategory === 'comics' ? '#F2D335' : 'white'} className="searchIcon user" />
-        </div>
+        <HeadersIcons
+          onClickCharacters={onClickCharacters} 
+          onClickComics={onClickComics} 
+          searchCategory={searchCategory} 
+        />
       </div>
     </div>
   );
