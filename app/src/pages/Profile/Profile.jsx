2edf7e49cf5699feb7666b40356f06eaa696a736
LeandroParisi <lepcbelisario@gmail.com>
2021-04-13 01:03:51 -0300

    feat: finishes site

--- app/src/pages/Profile/Profile.jsx
+++ app/src/pages/Profile/Profile.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { toast } from 'react-toastify';
-import { useLocation } from 'react-router';
+import { useHistory, useLocation } from 'react-router';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import UserForm from '../../components/UserForm/UserForm';
 import useProtectRoute from '../../hooks/useProtectRoute';
 import editUser from '../../services/editUser';
-import Header from '../../components/Header/Header';
+import './Profile.scss';
 
 function Profile() {
   const { pathname } = useLocation();
+  const history = useHistory();
 
   const handleButtonClick = async (payload) => {
     const { status, message } = await editUser(payload);
@@ -20,8 +23,10 @@ function Profile() {
   return (
     <>
       { useProtectRoute() }
-      <Header page={pathname} />
       <UserForm onSubmit={handleButtonClick} page={pathname} />
+      <div className="closeProfile" onClick={() => history.push('/search')} role="button" tabIndex={0} onKeyDown={() => history.push('/search')}>
+        <FontAwesomeIcon icon={faArrowLeft} color="white" size="2x" />
+      </div>
     </>
   );
 }
