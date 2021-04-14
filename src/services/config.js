0ec8e1356e0196ad89a02ec3f9b8f085a59c697a
LeandroParisi <lepcbelisario@gmail.com>
2021-04-13 22:52:52 -0300

    fix: changes default server on backend

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const url = process.env.BACKEND_SERVER || 'http://localhost:3001';
+const url = process.env.BACKEND_SERVER || 'https://marvelousapp-front.herokuapp.com';
 
 export const endpoints = {
   login: '/users/login',
