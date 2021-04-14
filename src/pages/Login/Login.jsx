dc9c82162b84bd3035df3881de83224a09346419
LeandroParisi <lepcbelisario@gmail.com>
2021-04-14 18:57:46 -0300

    feat: adds useLoading to Login and register

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory, useLocation } from 'react-router';
 // import { toast } from 'react-toastify';
 import useLoader from '../../hooks/useLoader';
@@ -7,17 +7,22 @@ import postLogin from '../../services/postLogin';
 // import { saveToken, saveUser } from '../../store/localStorage/actions';
 import UserForm from '../../components/UserForm/UserForm';
 import { dispatchUserAction } from '../../services/utils';
+import useLoading from '../../hooks/useLoading';
 
 function Login() {
   const history = useHistory();
   const { pathname } = useLocation();
+  const [isFetching, setIsFetching] = useState(false);
 
   const handleButtonClick = async (payload) => {
-    dispatchUserAction(postLogin, payload, history);
+    setIsFetching(true);
+    await dispatchUserAction(postLogin, payload, history);
+    setIsFetching(false);
   };
 
   return (
     <>
+      { useLoading(isFetching) }
       { useLoader('main1') }
       <UserForm onSubmit={handleButtonClick} page={pathname} />
     </>
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory, useLocation } from 'react-router';
 import UserForm from '../../components/UserForm/UserForm';
 import useProtectRoute from '../../hooks/useProtectRoute';
 import { dispatchUserAction } from '../../services/utils';
 import saveUser from '../../services/saveUser';
+import useLoading from '../../hooks/useLoading';
 
 function Register() {
   const history = useHistory();
   const { pathname } = useLocation();
+  const [isFetching, setIsFetching] = useState(false);
 
   const handleButtonClick = async (payload) => {
+    setIsFetching(true);
     dispatchUserAction(saveUser, payload, history);
+    setIsFetching(false);
   };
 
   return (
     <>
+      { useLoading(isFetching) }
       { useProtectRoute() }
       <UserForm onSubmit={handleButtonClick} page={pathname} />
     </>
